3441f13b195061afab5122d76e08ea1b61d64875
anit3573 <anit3573@CSL0688.global.chainsys.com>
2024-04-18 10:18:48 +0530

    Number Series

---
+++ CodingChallenge/src/com/chainsys/codingchallenge/Series.java
@@ -0,0 +1,25 @@
+package com.chainsys.codingchallenge;
+import java.util.*;
+import java.io.*;
+public class Series 
+{
+	public static void main(String []argh)
+	{
+		Scanner in = new Scanner(System.in);
+		int t=in.nextInt();
+		for(int i=0;i<t;i++)
+		{
+			int a = in.nextInt();
+			int b = in.nextInt();
+			int n = in.nextInt();
+			int result=a;
+			for(int j=0;j<n;j++)
+			{
+				result+=(Math.pow(2,j)*b);
+				System.out.print(result+" ");
+			}
+			System.out.println();
+		}
+		in.close();
+	}
+}
